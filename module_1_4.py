d93cca31b0e6c66708ffe0005e5660762acca26f
Hallu1 <pikamikame@gmail.com>
2024-10-10 22:55:41 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,19 @@
+my_string = input('О чем ты мечтаешь? ')
+print(len(my_string))
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(" ",""))
+print(my_string[0])
+print(my_string[-1])
+#тут я то что смогла сделать постаралась, но насколько я поняла,...
+#...для третьего задания надо просто присвоить текст для my_string
+#В общем, на всякий случай сделаю так тоже
+my_string = "Хочу миллион долларов"
+print(my_string)
+print(len(my_string)) #может тут это и не надо, но мне было интересно посчитается ли
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(' ',''))
+print(my_string[0])
+print(my_string[-1])
+print(my_string[20])
