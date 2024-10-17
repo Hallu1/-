b1ebeb4ae1e14b48c8b839965478d0b9f855329b
Hallu1 <pikamikame@gmail.com>
2024-10-18 01:42:14 +0300

    Add files via upload

---
+++ Модуль_2_3.ру.py
@@ -0,0 +1,128 @@
+while True: #по видеоуроку не совсем поняла, как мне выбирать, что писать после команды while. любое условие, которое верно?
+    my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+    if my_list[0] > 0:
+        print(my_list[0])
+    else:
+        print('Упс')
+        break
+    if my_list[1] > 0:
+        print(my_list[1])
+    else:
+        print('Упс')
+        break
+    if my_list[2] > 0:
+            print(my_list[2])
+    else:
+            print('Упс')
+            break
+    if my_list[3] > 0:
+            print(my_list[3])
+    else:
+            print('Упс')
+            break
+    if my_list[4] > 0:
+            print(my_list[4])
+    else:
+            print('Упс')
+            break
+    if my_list[5] > 0:
+        print(my_list[5])
+    else:
+        print('Упс')
+        break
+    if my_list[6] > 0:
+            print(my_list[6])
+    else:
+            print('Упс')
+            break
+    if my_list[7] > 0:
+        print(my_list[7])
+    else:
+        print('Упс')
+        break
+    if my_list[8] > 0:
+            print(my_list[8])
+    else:
+            print('Упс')
+            break
+    if my_list[9] > 0:
+            print(my_list[9])
+    else:
+            print('Упс')
+            break
+    if my_list[10] > 0:
+            print(my_list[10])
+    else:
+            print('Упс')
+            break
+    if my_list[11] > 0:
+            print(my_list[11])
+    else:
+            print('Упс')
+            break
+# мне кажется, решение получилось слишком длинное/сложное и я что-то недопоняла, но даже так я еле решила:D
+# Чтобы сделать так, как было указано в предполагаемом ответе в практическом задании, необходимо все условия поменять на
+# на >= 0. Но так как по заданию надо было вывести положительные числа, а 0 к ним не относится, я начала так.
+# На всякий случай ниже второе решение.
+while True:
+    my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+    if my_list[0] >= 0:
+        print(my_list[0])
+    else:
+        print('Упс')
+        break
+    if my_list[1] >= 0:
+        print(my_list[1])
+    else:
+        print('Упс')
+        break
+    if my_list[2] >= 0:
+            print(my_list[2])
+    else:
+            print('Упс')
+            break
+    if my_list[3] >= 0:
+            print(my_list[3])
+    else:
+            print('Упс')
+            break
+    if my_list[4] >= 0:
+            print(my_list[4])
+    else:
+            print('Упс')
+            break
+    if my_list[5] >= 0:
+        print(my_list[5])
+    else:
+        print('Упс')
+        break
+    if my_list[6] >= 0:
+            print(my_list[6])
+    else:
+            print('Упс')
+            break
+    if my_list[7] >= 0:
+        print(my_list[7])
+    else:
+        print('Упс')
+        break
+    if my_list[8] >= 0:
+            print(my_list[8])
+    else:
+            print('Упс')
+            break
+    if my_list[9] >= 0:
+            print(my_list[9])
+    else:
+            print('Упс')
+            break
+    if my_list[10] >= 0:
+            print(my_list[10])
+    else:
+            print('Упс')
+            break
+    if my_list[11] >= 0:
+            print(my_list[11])
+    else:
+            print('Упс')
+            break
